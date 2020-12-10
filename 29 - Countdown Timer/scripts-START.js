67e9ede23956a4d0ef43837b035e74541ffcfd5b
Stefan Sarow <stefan@pixelaffairs.com>
2020-12-10 20:33:13 +0100

    day 29

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,11 +1,16 @@
+let countdown;
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
+
 function timer(seconds) {
+  clearInterval(countdown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
-  console.log({ now, then });
-
-  const countdown = setInterval(() => {
+  countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     if (secondsLeft < 0) {
       clearInterval(countdown);
@@ -18,5 +23,28 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   seconds = seconds % 60;
-  console.log(minutes, seconds);
+  const display = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  timerDisplay.textContent = display;
+  document.title = display;
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hours = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `${hours}:${minutes < 10 ? "0" : ""}${minutes}`;
+}
+
+function setTime() {
+  timer(this.dataset.time);
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", setTime);
+});
+
+document.customForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  timer(this.minutes.value * 60);
+  this.reset();
+});
